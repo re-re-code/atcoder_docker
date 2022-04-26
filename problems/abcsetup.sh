ce26e0ca713edc23294073c23c3dc7e58320b056
re-re-code <102694250+re-re-code@users.noreply.github.com>
2022-04-26 12:27:59 +0900

    Update Usability

--- problems/abcsetup.sh
+++ problems/abcsetup.sh
@@ -11,19 +11,17 @@ if [ ! -e ABC ]; then
 fi
 
 mkdir -p ABC/$1
-len=`echo $2`
-i=1
 for alpha in a b c d e f g h
 do
-  cur=`echo $2 | cut -c $i`
-  if [ ${#len} -ge $i ]; then
-    if [ -n $cur ]; then
+  for i in 1 2 3 4 5 6 7 8
+  do
+    cur=`echo $2 | cut -c $i`
+    if [ ${#cur} -ge 1 ]; then
       if [ $cur = $alpha ]; then
         cp -n template.cpp ABC/$1/$cur.cpp
       fi
     fi
-  fi
-  i=$(($i+1))
+  done
 done
 
 exit 0
